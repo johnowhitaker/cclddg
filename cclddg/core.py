ba686036b54da0423afeeb9c2b8b0b4e5abda1b2
Jonathan Whitaker <johnowhitaker@gmail.com>
2022-05-05 03:26:02 +0000

    Added visualization of TimeEmbedding in action and tidies docstrings for some other components.

--- cclddg/core.py
+++ cclddg/core.py
@@ -11,6 +11,7 @@ from torch import nn
 
 # Cell
 class Swish(nn.Module):
+    """ *swish...* :) """
     def forward(self, x):
         return x * torch.sigmoid(x)
 
@@ -18,16 +19,15 @@ class Swish(nn.Module):
 
 # The time embedding
 class TimeEmbedding(nn.Module):
-    """
-    ### Embeddings for $t$
-    """
+    """ Embeddings for $t$ """
 
-    def __init__(self, n_channels: int):
+    def __init__(self, n_channels: int, denom_factor=10000):
         """
         * `n_channels` is the number of dimensions in the embedding
         """
         super().__init__()
         self.n_channels = n_channels
+        self.denom_factor = denom_factor
         # First linear layer
         self.lin1 = nn.Linear(self.n_channels // 4, self.n_channels)
         # Activation
@@ -35,21 +35,17 @@ class TimeEmbedding(nn.Module):
         # Second linear layer
         self.lin2 = nn.Linear(self.n_channels, self.n_channels)
 
-    def forward(self, t: torch.Tensor):
-        # Create sinusoidal position embeddings
-        # [same as those from the transformer](../../transformers/positional_encoding.html)
-        #
-        # \begin{align}
-        # PE^{(1)}_{t,i} &= sin\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg) \\
-        # PE^{(2)}_{t,i} &= cos\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg)
-        # \end{align}
-        #
-        # where $d$ is `half_dim`
+    def forward(self, t: torch.Tensor, return_sinusoidal_embs=False):
+        """ Create sinusoidal position embeddings as used in many transformers. """
         half_dim = self.n_channels // 8
-        emb = math.log(10_000) / (half_dim - 1)
+        emb = math.log(self.denom_factor) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, device=t.device) * -emb)
         emb = t[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=1)
+
+        if return_sinusoidal_embs:
+            return emb
+
 
         # Transform with the MLP
         emb = self.act(self.lin1(emb))
@@ -61,6 +57,7 @@ class TimeEmbedding(nn.Module):
 # Cell
 # CLOOB embedding
 class CLOOBEmbedding(nn.Module):
+    """ Embedding to map a CLOOB embedding (512 dimensions) to n_channels via an MLP """
     def __init__(self, n_channels: int):
         super().__init__()
         self.n_channels = n_channels
@@ -76,6 +73,7 @@ class CLOOBEmbedding(nn.Module):
 
 # One for the gan latent, z
 class ZEmbedding(nn.Module):
+    """ Embedding to map a CLOOB embedding (`z_dim` dimensions) to n_channels via an MLP """
     def __init__(self, z_dim:int, n_channels: int):
         super().__init__()
         self.n_channels = n_channels
@@ -276,7 +274,7 @@ class MiddleBlock(nn.Module):
 
 class Upsample(nn.Module):
     """
-    ### Scale up the feature map by $2 \times$
+    ### Scale up the feature map by 2.
     """
 
     def __init__(self, n_channels):
@@ -292,7 +290,7 @@ class Upsample(nn.Module):
 
 class Downsample(nn.Module):
     """
-    ### Scale down the feature map by $\frac{1}{2} \times$
+    ### Scale down the feature map by 0.5
     """
 
     def __init__(self, n_channels):
